8c68a6d6de5d59698c2989d14d1e93c649e14f55
ReDMallard <marco@titori.it>
2023-10-30 20:39:46 +0100

    primo tentativo di layout

--- js/main.js
+++ js/main.js
@@ -6,16 +6,16 @@ for (let i = 0; i <= 100; i++) {
 
     if ((resto3 == 0) && (resto5 == 0)) {
         console.log(i)
-        container.innerHTML = container.innerHTML + `<p class="orange">Valore: ${i}</p>`;
+        container.innerHTML = container.innerHTML + `<div class="col-2 orange">Valore: ${i}</div>`;
     } else if (resto3 == 0) {
         console.log(i)
-        container.innerHTML = container.innerHTML + `<p class="red">Valore: ${i}</p>`;
+        container.innerHTML = container.innerHTML + `<div class="col-2 red">Valore: ${i}</div>`;
     } else if (resto5 == 0) {
         console.log(i)
-        container.innerHTML = container.innerHTML + `<p class="green">Valore: ${i}</p>`;
+        container.innerHTML = container.innerHTML + `<div class="col-2 green">Valore: ${i}</div>`;
     } else {
         console.log(i)
-        container.innerHTML = container.innerHTML + `<p>Valore: ${i}</p>`;
+        container.innerHTML = container.innerHTML + `<div class="col-2">Valore: ${i}</div>`;
     }
 
 }
