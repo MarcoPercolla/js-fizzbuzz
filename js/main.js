5195b04b3084b1e82af8039d986f844f8717efde
ReDMallard <marco@titori.it>
2023-10-31 02:06:22 +0100

    all done

--- js/main.js
+++ js/main.js
@@ -1,21 +1,32 @@
 const container = document.getElementById("container");
 
-for (let i = 0; i <= 100; i++) {
+for (let i = 1; i <= 100; i++) {
     let resto3 = (i % 3);
     let resto5 = (i % 5);
+    const newDiv = document.createElement("div");
+    newDiv.innerHTML = `${i}`;
+    container.append(newDiv);
+    newDiv.classList.add("square");
+    
 
     if ((resto3 == 0) && (resto5 == 0)) {
-        console.log(i)
-        container.innerHTML = container.innerHTML + `<div class="col-2 orange">Valore: ${i}</div>`;
+        console.log(i);
+        newDiv.innerHTML = `BuzzFizz`;
+        newDiv.classList.add("orange");
+        
     } else if (resto3 == 0) {
-        console.log(i)
-        container.innerHTML = container.innerHTML + `<div class="col-2 red">Valore: ${i}</div>`;
+        console.log(i);
+        newDiv.innerHTML = `Buzz`;
+        newDiv.classList.add("red");
+        
     } else if (resto5 == 0) {
-        console.log(i)
-        container.innerHTML = container.innerHTML + `<div class="col-2 green">Valore: ${i}</div>`;
+        console.log(i);
+        newDiv.innerHTML = `Fizz`;
+        newDiv.classList.add("green");
+        
     } else {
-        console.log(i)
-        container.innerHTML = container.innerHTML + `<div class="col-2">Valore: ${i}</div>`;
+        console.log(i);
+        newDiv.classList.add("white");
     }
 
 }
